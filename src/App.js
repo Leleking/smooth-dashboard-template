b8bb14ca52110dae50226ac637c0e3240e83f78c
Simeon Nortey <simeonnortey@gmail.com>
2022-10-08 21:42:12 +0000

    changed basename in production

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <>
       <BrowserRouter
         basename={
-          process.env.NODE_ENV || process.env.NODE_ENV === "development"
+          !process.env.NODE_ENV || process.env.NODE_ENV === "development"
             ? ""
             : "smooth-dashboard-template"
         }
