25b941ef8e9e33c8dccd935e4c99afb163849b39
Simeon Nortey <simeonnortey@gmail.com>
2022-10-08 12:34:37 +0000

    updated modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { BottomSheet } from "react-spring-bottom-sheet";
+import { Dialog, Transition } from "@headlessui/react";
 import "react-spring-bottom-sheet/dist/style.css";
 
-export default function Modal({ isOpen, onDismiss, children, ...rest }) {
+export default function Modal({
+  isOpen,
+  onDismiss,
+  children,
+  footer,
+  ...rest
+}) {
   const [deviceSize, setDeviceSize] = useState(window.innerWidth);
 
   useEffect(() => {
@@ -15,10 +22,58 @@ export default function Modal({ isOpen, onDismiss, children, ...rest }) {
 
   if (deviceSize <= 480) {
     return (
-      <BottomSheet open={isOpen} onDismiss={() => onDismiss()} {...rest}>
+      <BottomSheet
+        open={isOpen}
+        onDismiss={() => onDismiss()}
+        footer={footer}
+        {...rest}
+      >
         {children}
       </BottomSheet>
     );
   }
-  return <div></div>;
+  return (
+    <>
+      <Transition.Root show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={onDismiss}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        {children}
+                      </div>
+                    </div>
+                  </div>
+                  {footer ? <div>{footer}</div> : null}
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
+  );
 }
